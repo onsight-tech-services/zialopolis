3ba8b3e632122ef949f76365280965bad7f2c9a2
maximaeus <jones.murphy@onsight.services>
2023-03-07 06:22:36 +0000

    wireguard init script

--- setup_wireguard.sh
+++
@@ -1,6 +0,0 @@
-wg genkey > /data/private
-wg pubkey < /data/private > /data/public
-ip -6 link add wg0 type wireguard
-ip addr add fe80::1/64 dev wg0
-wg set wg0 private-key /data/private
-ip -6 link set wg0 up
--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
+chmod +x wireguard/init
 docker volume create wireguard-data
 docker volume create mongo-data
 docker compose up -d
