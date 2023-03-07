f18cfe5cc83afe854cf97bef5aa179572d2f5bd9
maximaeus <jones.murphy@onsight.services>
2023-03-07 06:02:06 +0000

    testing wireguard

--- setup_wireguard.sh
+++ setup_wireguard.sh
@@ -1,3 +1,6 @@
 wg genkey > /data/private
 wg pubkey < /data/private > /data/public
-ip -p link add wg0 type wireguard
+ip -6 link add wg0 type wireguard
+ip addr add fe80::1/64 dev wg0
+wg set wg0 private-key /data/private
+ip -6 link set wg0 up
